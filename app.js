cb98d8f1fdc24bce0b965a37fea3df85aee0814e
coderken <jk0910948659@gmail.com>
2022-05-21 21:21:49 +0800

    firsy commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// console.log(12);
+// console.log(23);
+
+// console.log('log');
+
+// let arr = ['frontend', 'backend', 'devops'];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i], i);
+// }
+
+// arr.forEach((i, idx) => {
+//   console.log(idx, i);
+// });
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(12, 8));
+
+class circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  getArea() {
+    return this.radius * this.radius * Math.PI;
+  }
+}
+
+let c1 = new circle(8);
+console.log(c1.radius);
+console.log(c1.getArea());
