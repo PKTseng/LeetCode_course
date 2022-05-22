0707e1935a5122fa3c146eb8e6e58963aecdffbd
coderken <jk0910948659@gmail.com>
2022-05-22 23:26:30 +0800

    update

--- app.js
+++ app.js
@@ -32,3 +32,36 @@
 // let c1 = new circle(8);
 // console.log(c1.radius);
 // console.log(c1.getArea());
+
+// 時間跟空間的差別
+
+// ex: 1+2+3+4+5+....+n
+
+function fun1(n) {
+  let sun = 0;
+  for (let i = 0; i <= n; i++) {
+    sun += i;
+  }
+  return sun;
+}
+
+function fun2(n) {
+  return ((1 + n) * n) / 2;
+}
+
+// console.log(fun1(10));
+// console.log(fun2(10));
+
+let time1 = window.performance.now();
+fun1(10000000);
+let time2 = window.performance.now();
+
+let timeDiff1 = (time2 - time1) / 1000;
+console.log(`It takes ${timeDiff1} seconds to run fun1.`);
+
+let time3 = window.performance.now();
+fun2(10000000);
+let time4 = window.performance.now();
+
+let timeDiff2 = (time4 - time3) / 1000;
+console.log(`It takes ${timeDiff2} seconds to run fun2.`);
